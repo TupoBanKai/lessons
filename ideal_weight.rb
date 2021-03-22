1b5e0fd098286aa08071718549c923b5263b63b9
BanKai <leagueoflegends.talk@gmail.com>
2021-03-22 23:44:49 +0300

    lesson 1

---
+++ ideal_weight.rb
@@ -0,0 +1,18 @@
+class WeighingMachine
+
+	def initialize(name, growth)
+		@name = name
+		@growth = growth
+	end
+
+	def starter
+		if @growth < 110
+			print "Your weight is already optimal"
+		else
+			print "#{@name}, your optimal weight is #{(@growth - 110) *1.15}"
+		end
+	end
+end
+print "Enter name and height: "
+weighing_machine = WeighingMachine.new(gets.chomp, gets.chomp.to_i)
+weighing_machine.starter
