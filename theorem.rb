81d0ff7f3d50f3c7cca58b490e8d7b01adcf56ac
BanKai <leagueoflegends.talk@gmail.com>
2021-03-22 23:50:18 +0300

    lesson 1, homework 4

---
+++ theorem.rb
@@ -0,0 +1,34 @@
+class Theorem
+
+	def initialize first_leg, second_leg, hypotenuse
+		@first_leg = first_leg
+		@second_leg = second_leg
+		@hypotenuse = hypotenuse
+		@values = [first_leg, second_leg, hypotenuse].uniq.sort
+	end
+
+	def isosceles
+		if @values.length == 2
+			print "The triangle is isosceles"
+		else equilateral
+		end
+	end
+
+	def equilateral
+		if @values.length == 1
+			print "The triangle is equilateral"
+		else right_angle
+		end
+	end
+
+	def right_angle
+		if !(@hypotenuse > @first_leg && @hypotenuse > @second_leg)
+			print "You have specified the wrong hypotenuse"
+		else @first_leg**2 + @second_leg**2 == @hypotenuse**2
+			print "The triangle is right-angled"
+		end
+	end
+end
+print "Enter the sides of the triangle: "
+theorem = Theorem.new(gets.chomp.to_i, gets.chomp.to_i, gets.chomp.to_i)
+theorem.isosceles
