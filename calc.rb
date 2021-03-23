69c1884f04ca6afdc51b1322481bf57e471c64ee
BanKai <leagueoflegends.talk@gmail.com>
2021-03-23 19:23:05 +0300

    Retake

--- calc.rb
+++ calc.rb
@@ -1,14 +1,12 @@
 class Calc
-
-	def initialize a, h
-		@a = a
-		@h = h
-	end
-
-	def calculate
-		print 1.0/2*@a*@h
-	end
+  def initialize(a, h)
+    @a = a
+    @h = h
+  end
+  def calculate
+    print 1.0/2 * @a * @h
+  end
 end
-print "Enter the base and height of the triangle: "
-calc = Calc.new(gets.chomp.to_i, gets.chomp.to_i)
+print 'Enter the base and height of the triangle: '
+calc = Calc.new(gets.to_i, gets.to_i)
 calc.calculate
--- ideal_weight.rb
+++ ideal_weight.rb
@@ -1,18 +1,16 @@
 class WeighingMachine
-
-	def initialize(name, growth)
-		@name = name
-		@growth = growth
-	end
-
-	def starter
-		if @growth < 110
-			print "Your weight is already optimal"
-		else
-			print "#{@name}, your optimal weight is #{(@growth - 110) *1.15}"
-		end
-	end
+  def initialize(name, growth)
+    @name = name
+      @growth = growth
+  end
+  def starter
+    if @growth < 110
+      print 'Your weight is already optimal'
+    else
+      print "#{@name}, your optimal weight is #{(@growth - 110) * 1.15}"
+    end
+  end
 end
-print "Enter name and height: "
-weighing_machine = WeighingMachine.new(gets.chomp, gets.chomp.to_i)
+print 'Enter name and height: '
+weighing_machine = WeighingMachine.new(gets.chomp, gets.to_i)
 weighing_machine.starter
