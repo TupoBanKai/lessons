0b2a389fa526ccd057c8db24a743781d46fec77d
BanKai <leagueoflegends.talk@gmail.com>
2021-03-26 19:55:56 +0300

    home work 2

--- HomeWork2/alphabet.rb
+++ HomeWork2/alphabet.rb
@@ -2,6 +2,6 @@ alphabet = 'a b c d e f g h i j k l m n o p q r s t u v w x y z'.split(' ')
 vowls = 'a e i o u y'.split(' ')
 hash = Hash.new
 vowls.each do |value|
-	hash[value] = alphabet.find_index(value) + 1
+  hash[value] = alphabet.find_index(value) + 1
 end
 print hash
--- HomeWork2/calendar.rb
+++ HomeWork2/calendar.rb
@@ -1,49 +1,49 @@
 class Calendar
-	MONTHS = {
-		1 => 31,
-		2 => 28,
-		3 => 31,
-		4 => 30,
-		5 => 31,
-		6 => 30,
-		7 => 31,
-		8 => 31,
-		9 => 30,
-		10 => 31,
-		11 => 30,
-		12 => 31
-		}
+  MONTHS = {
+    1 => 31,
+    2 => 28,
+    3 => 31,
+    4 => 30,
+    5 => 31,
+    6 => 30,
+    7 => 31,
+    8 => 31,
+    9 => 30,
+    10 => 31,
+    11 => 30,
+    12 => 31
+    }
 
-	def initialize(day, month, year)
-		@day = day
-		@month = month
-		@year = year
-		@i = 0
-	end
+  def initialize(day, month, year)
+    @day = day
+    @month = month
+    @year = year
+    @i = 0
+  end
 
-	def find_number_day
-		MONTHS.select do |key, value| 
-			if key == @month
-				@i += value
-				break
-			else
-				@i += value
-			end
-		end
-		leap_year(@i)
-	end
+  def find_number_day
+    MONTHS.select do |key, value| 
+      if key == @month
+        @i += value
+        break
+      else
+        @i += value
+      end
+    end
+    leap_year(@i)
+  end
 
-	def leap_year(i)
-		i -= @month - @day
-		if @year % 4 == 0
-			i += 1
-		elsif @year % 100 == 0
-			i -= 1
-		elsif @year % 400 == 0
-			i += 1 
-		end
-		print i
-	end
+  def leap_year(i)
+    i -= @month - @day
+    if @year % 4 == 0
+      i += 1
+    elsif @year % 100 == 0
+      i -= 1
+    elsif @year % 400 == 0
+      i += 1 
+    end
+    print i
+  end
 end
 calendar = Calendar.new(gets.to_i, gets.to_i, gets.to_i)
 calendar.find_number_day
