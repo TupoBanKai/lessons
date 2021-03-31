a023432d82490bb98a7ae51670ae58cf253cac1f
BanKai <leagueoflegends.talk@gmail.com>
2021-03-31 19:26:28 +0300

    Home Work 34

--- HomeWork3/train.rb
+++ HomeWork3/train.rb
@@ -1,47 +1,34 @@
-class Train
-  attr_reader :speed, :carriages
+class Station
+  attr_reader :current_trains, :station_type_trains
 
-  def initialize(number, type, carriages)
-    @number = number
-    @type = type
-    @carriages = carriages
-    @speed = 0
+  def initialize(name_station)
+    @name_station = name_station
+    @current_trains = []
+    @station_type_trains = {}
   end
 
-  def take_overclocking
-    if @type == 'Passangers'
-      puts @speed = 80
-    elsif @type == 'Cargo'
-      puts @speed = 60
-    end
+  def receiving_trains(train_type)
+      @current_trains << train_type
+      @station_type_trains[train_type] = @station_type_trains[train_type].to_i + 1
   end
 
-  def hit_the_brake
-    puts @speed = 0
-  end
-
-  def attach_carriage
-    puts @carriages += 1
-  end
-
-  def unhook_carriage
-    puts @carriages -= 1
-  end
-
-  def departure(rout)
-    puts "Train went on the route - #{rout}"
+  def sending_trains
+    @current_trains.delete(0)
   end
 end
 
-class Rout #тот же класс, что и в файле rout.rb
+
+
+class Route
+  attr_reader :start_station, :end_station
   def initialize(start_station, end_station)
     @start_station = start_station
     @end_station = end_station
     @way = []
   end
 
-  def add_intermediate_station(name)
-    @way << name
+  def add_intermediate_station(statin)
+    @way << statin
   end
 
   def delete_station(name)
@@ -52,15 +39,78 @@ class Rout #тот же класс, что и в файле rout.rb
     end
   end
 
-  def show_stations
-    @way.unshift(@start_station).push(@end_station)
+  def stations
+    return [@start_station, *@way, @end_station]
   end
 end
 
-train = Train.new(1, 'Cargo', 10)
-rout = Rout.new('Volga', 'NeVolga')
-train.take_overclocking
-train.hit_the_brake
-train.attach_carriage
-train.carriages
-train.departure(rout.show_stations)
+
+
+class Train
+  attr_reader :speed, :carriages
+
+  def initialize(number, type, carriages)
+    @number = number
+    @type = type
+    @carriages = carriages
+    @speed = 0
+    @route = nil
+    @current_station = nil
+  end
+
+  def take_overclocking
+    return @speed = 60
+  end
+
+  def hit_the_brake
+    return @speed = 0
+  end
+
+  def attach_carriage
+    if @speed == 0
+      return @carriages += 1
+    else
+      puts 'You need hit_the_brake'
+    end
+  end
+
+  def unhook_carriage
+    if @speed == 0
+      return @carriages -= 1
+    else
+      puts 'You need hit_the_brake'
+    end
+  end
+
+  def set_route(route)
+    @route = route.stations
+    @current_station = @route.first
+    @current_station.receiving_trains(@type)
+  end
+
+  def move_next
+    if @current_station != @route.last
+      @current_station.sending_trains
+      @route[@route.find_index(@current_station) + 1].receiving_trains(@type)
+    else
+      puts 'You have already arrived at your destination'
+    end
+  end
+
+  def move_previos
+    if @current_station != @route.first
+      @current_station.sending_trains
+      @route.stations[@route.find_index(@current_station) - 1].receiving_trains(@type)
+    else
+      puts 'You are already at the starting point of the route'
+    end
+  end
+end
+
+
+station = Station.new('Volga')
+station2 = Station.new('NeVolga')
+route = Route.new(station, station2)
+train = Train.new(10, 'Cargo', 20)
+train.set_route(route)
+train.move_next
